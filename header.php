0a5c635383cf247c2a8ac8da97ea489125885f2b
EngM7md4Ever <63129938+EngM7md4Ever@users.noreply.github.com>
2020-06-14 21:12:12 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+include 'dbh.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
